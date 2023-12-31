a69fc07992208175503d8dbcbaea8ab24bb3cc6d
Emp 000 <emp000@myworkaholic.com>
2023-12-31 20:20:09 +0530

    Added 1st Jan

--- views/_day-jan-01.php
+++ views/_day-jan-01.php
@@ -1,19 +1,20 @@
 <?php
 /* @var $this \yii\web\View */
+
+$currentYear = date("Y"); // Get the current year
+$nextYear = $currentYear + 1; // Calculate the next year
 ?>
 
 <div class="special-day-loader-wrapper">
     <div class="loading">
-        <h2>2018</h2>
+        <h2><?php echo $currentYear; ?></h2> <!-- Display current year -->
         <div class="bar"></div>
-        <h2>2019</h2>
+        <h2><?php echo $nextYear; ?></h2> <!-- Display next year -->
     </div>
     <div class="center">
         <div><h2>Happy New Year</h2></div>
         <div></div>
-        <div><h2>2019</h2></div>
+        <div><h2><?php echo $nextYear; ?></h2></div> <!-- Display next year -->
         <div></div>
     </div>
 </div>
-
-
